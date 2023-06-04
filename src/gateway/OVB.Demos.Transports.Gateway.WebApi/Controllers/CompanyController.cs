dc961863563a0e34c955fa4ea7b07e3bafea6840
OtavioVB <otaviovb.developer@gmail.com>
2023-06-04 19:05:24 -0300

    base: controllers base for create companies

---
+++ src/gateway/OVB.Demos.Transports.Gateway.WebApi/Controllers/CompanyController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace OVB.Demos.Transports.Gateway.WebApi.Controllers;
+
+[ApiVersion("1")]
+[Route("api/gateway/v1/management/[controller]")]
+[ApiController]
+public sealed class CompanyController : ControllerBase
+{
+    [HttpPost]
+    [Route("Create")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+    [AllowAnonymous]
+    public async Task<IActionResult> CreateAsync(CancellationToken cancellationToken)
+    {
+        throw new Exception();
+    }
+
+    [HttpPost]
+    [Route("BatchImport")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+    [AllowAnonymous]
+    public async Task<IActionResult> BatchImportAsync(CancellationToken cancellationToken)
+    {
+        throw new Exception();
+    }
+}
