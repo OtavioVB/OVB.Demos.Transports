9102d40ce7acecf07e21b9870c9d2f537ae99674
OtavioVB <otaviovb.developer@gmail.com>
2023-06-04 19:30:01 -0300

    feat: `libraries: data transfer object base`

---
+++ libs/OVB.Demos.Libraries.Domain/DataTransferObject.cs
@@ -0,0 +1,38 @@
+﻿namespace OVB.Demos.Libraries.Domain;
+
+public static class DataTransferObjectBase
+{
+    public abstract class Default
+    {
+        protected Default(Guid identifier)
+        {
+            Identifier = identifier;
+        }
+
+        public Guid Identifier { get; set; }
+    }
+
+    public abstract class Time
+    {
+        protected Time(DateTime createdAt, DateTime updatedAt)
+        {
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public abstract class MicrosservicesInfo
+    {
+        protected MicrosservicesInfo(Guid correlationIdentifier, string sourcePlatform)
+        {
+            CorrelationIdentifier = correlationIdentifier;
+            SourcePlatform = sourcePlatform;
+        }
+
+        public Guid CorrelationIdentifier { get; set; }
+        public string SourcePlatform { get; set; }
+    }
+}
--- src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Domain/Bussines/OwnerPhoneContext/DataTransferObject/OwnerPhone.cs
+++ src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Domain/Bussines/OwnerPhoneContext/DataTransferObject/OwnerPhone.cs
@@ -1,16 +1,16 @@
-﻿namespace OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerPhoneContext.DataTransferObject;
+﻿using OVB.Demos.Libraries.Domain;
 
-public sealed class OwnerPhone
+namespace OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerPhoneContext.DataTransferObject;
+
+public sealed class OwnerPhone : DataTransferObjectBase.Default
 {
-    public OwnerPhone(Guid identifier, string ddi, string dd, string number)
+    public OwnerPhone(Guid identifier, string ddi, string dd, string number) : base(identifier)
     {
-        Identifier = identifier;
         Ddi = ddi;
         Dd = dd;
         Number = number;
     }
 
-    public Guid Identifier { get; init; }
     public string Ddi { get; set; }
     public string Dd { get; set; }
     public string Number { get; init; }
