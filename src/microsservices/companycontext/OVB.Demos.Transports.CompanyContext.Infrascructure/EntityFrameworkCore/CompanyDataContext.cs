e9b45fa185ef86f261c4f7085a0c0067c6b0a615
OtavioVB <otaviovb.developer@gmail.com>
2023-06-04 20:36:22 -0300

    feat: `entity framework core base`

---
+++ src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Infrascructure/EntityFrameworkCore/CompanyDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OVB.Demos.Libraries.EntityFrameworkCore.Base;
+using OVB.Demos.Transports.CompanyContext.Domain.Bussines.CompanyContext.DataTransferObject;
+using OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerContext.DataTransferObject;
+using OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerPhoneContext.DataTransferObject;
+
+namespace OVB.Demos.Transports.CompanyContext.Infrascructure.EntityFrameworkCore;
+
+public sealed class CompanyDataContext : EntityFrameworkDataContextBase
+{
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<OwnerPhone> OwnerPhones { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+
+    public CompanyDataContext(string connectionString) : base(connectionString)
+    {
+    }
+
+    protected override void OnConfiguringInternal(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(ConnectionString, p => p.MigrationsAssembly("OVB.Demos.Transports.CompanyContext.Infrascructure"));
+    }
+
+    protected override void OnModelCreatingInternal(ModelBuilder modelBuilder)
+    {
+        throw new NotImplementedException();
+    }
+}
