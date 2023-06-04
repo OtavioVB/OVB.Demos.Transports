4e186db8829ed6f4beb57d17d36279d03b8626e8
OtavioVB <otaviovb.developer@gmail.com>
2023-06-04 20:39:31 -0300

    feat: `owner authentication`

---
+++ src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Domain/Bussines/OwnerAuthenticationContext/DataTransferObject/OwnerAuthentication.cs
@@ -0,0 +1,23 @@
+﻿using OVB.Demos.Libraries.Domain;
+
+namespace OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerAuthenticationContext.DataTransferObject;
+
+public sealed class OwnerAuthentication : DataTransferObjectBase.All
+{
+    public OwnerAuthentication(string email, string username, string password, bool isEmailConfirmed, bool isActivatedAccess, 
+        Guid identifier, Guid correlationIdentifier, string sourcePlatform, DateTime createdAt, DateTime updatedAt) 
+        : base(identifier, correlationIdentifier, sourcePlatform, createdAt, updatedAt)
+    {
+        Email = email;
+        Username = username;
+        Password = password;
+        IsEmailConfirmed = isEmailConfirmed;
+        IsActivatedAccess = isActivatedAccess;
+    }
+
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool IsEmailConfirmed { get; set; }
+    public bool IsActivatedAccess { get; set; }
+}
--- src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Infrascructure/EntityFrameworkCore/CompanyDataContext.cs
+++ src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Infrascructure/EntityFrameworkCore/CompanyDataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OVB.Demos.Libraries.EntityFrameworkCore.Base;
 using OVB.Demos.Transports.CompanyContext.Domain.Bussines.CompanyContext.DataTransferObject;
+using OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerAuthenticationContext.DataTransferObject;
 using OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerContext.DataTransferObject;
 using OVB.Demos.Transports.CompanyContext.Domain.Bussines.OwnerPhoneContext.DataTransferObject;
 
@@ -11,6 +12,7 @@ public sealed class CompanyDataContext : EntityFrameworkDataContextBase
     public DbSet<Company> Companies { get; set; }
     public DbSet<OwnerPhone> OwnerPhones { get; set; }
     public DbSet<Owner> Owners { get; set; }
+    public DbSet<OwnerAuthentication> OwnerAuthentication { get; set; }
 
     public CompanyDataContext(string connectionString) : base(connectionString)
     {
