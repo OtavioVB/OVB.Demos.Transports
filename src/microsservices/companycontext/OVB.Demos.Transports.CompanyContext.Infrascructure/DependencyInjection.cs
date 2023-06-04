d96fe2cfa352485f4c5f9c59b4e7a9348da303a9
OtavioVB <otaviovb.developer@gmail.com>
2023-06-04 20:30:44 -0300

    feat: `entity framework base library`

---
+++ src/microsservices/companycontext/OVB.Demos.Transports.CompanyContext.Infrascructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OVB.Demos.Transports.CompanyContext.Infrascructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMicrosservicesCompanyContextInfrascructureDependenciesConfiguration(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
