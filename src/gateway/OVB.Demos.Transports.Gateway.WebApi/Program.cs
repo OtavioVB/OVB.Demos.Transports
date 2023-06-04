5371456fb0d6518ef221948211e92fcd40e5962c
OtavioVB <otaviovb.developer@gmail.com>
2023-06-04 10:52:11 -0300

    feat: `create gateway webapi`

---
+++ src/gateway/OVB.Demos.Transports.Gateway.WebApi/Program.cs
@@ -0,0 +1,20 @@
+
+namespace OVB.Demos.Transports.Gateway.WebApi;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
