8a0481dd445cc7997f66cf9e17fdc5821842c564
OtavioVB <otaviovb.developer@gmail.com>
2023-06-08 11:55:30 -0300

    feat: `management messages`

---
+++ libs/OVB.Demos.Transports.Responses/ManagementMessages/ManagementMessages.cs
@@ -0,0 +1,45 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OVB.Demos.Transports.Responses.ManagementMessages;
+
+public sealed class ManagementMessages<TEntity>
+{
+    private string EntityIdentifier { get; init; }
+    private IDictionary<string, IDictionary<string, ErrorMessage>> Messages { get; init; }
+
+    public ManagementMessages()
+    {
+        EntityIdentifier = nameof(TEntity);
+        Messages = new Dictionary<string, IDictionary<string, ErrorMessage>>();    
+    }
+
+    public void AddMessage(string code, string language, ErrorMessage errorMessage)
+    {
+        const string Exists = "This message for this code and language exists.";
+
+        var errorCodeMessageExists = Messages.ContainsKey(code);
+        if (errorCodeMessageExists == true)
+            if (Messages[code].ContainsKey(language))
+                throw new Exception(Exists);
+       
+        if(errorCodeMessageExists == false)
+            Messages.Add(code, new Dictionary<string, ErrorMessage>());
+
+        Messages[code].Add(language, errorMessage);
+    }
+
+    public ErrorMessage GetErrorMessageByLanguage(string code, string language)
+    {
+        const string Error = "This message for this code and language not exists.";
+
+        var errorCodeMessageExists = Messages.ContainsKey(code);
+        if (errorCodeMessageExists == false)
+            throw new Exception(Error);
+
+        var languageMessageExists = Messages[code].ContainsKey(language);
+        if (languageMessageExists == false)
+            throw new Exception(Error);
+
+        return Messages[code][language];
+    }
+}
