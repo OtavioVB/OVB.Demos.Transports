108aebd9c78caaff05be4b886629d837ac180149
OtavioVB <otaviovb.developer@gmail.com>
2023-06-15 20:54:01 -0300

    feat: company data transfer object

---
+++ src/OVB.Demos.Transports.Domain/CompanyContext/DataTransferObject/Company.cs
@@ -0,0 +1,19 @@
+﻿namespace OVB.Demos.Transports.Domain.CompanyContext.DataTransferObject;
+
+public record Company
+{
+    public Company(Guid identifier, string platformName, string realName, string cnpj, DateTime createdAt)
+    {
+        Identifier = identifier;
+        PlatformName = platformName;
+        RealName = realName;
+        Cnpj = cnpj;
+        CreatedAt = createdAt;
+    }
+
+    public Guid Identifier { get; init; }
+    public string PlatformName { get; init; }
+    public string RealName { get; init; }
+    public string Cnpj { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
